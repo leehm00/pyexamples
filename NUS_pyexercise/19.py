a53b7766ff5109152eeb1e586b01f0d9834095b9
leehm <iamgarrett365@gmail.com>
2020-06-23 11:29:21 +0800

    Default Changelist

--- NUS_pyexercise/19.py
+++ NUS_pyexercise/19.py
@@ -3,3 +3,15 @@
 # 开发时间 : 2020/2/23  22:16
 # 文件名称 : 19.py
 # 开发工具 : PyCharm
+a = input()
+b = input()
+c = input()
+d = int(input())
+e = int(input())
+if d == 1:
+    f = a.split()
+if d == 2:
+    f = b.split()
+if d == 3:
+    f = c.split()
+print(f[e-1])
